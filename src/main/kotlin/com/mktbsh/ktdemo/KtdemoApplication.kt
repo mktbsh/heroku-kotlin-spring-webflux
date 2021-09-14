fd599125aca85abcd5e40bf764171d033392425e
mktbsh <t93.hasebe@gmail.com>
2021-09-14 23:39:32 +0900

    Spring Initializer

---
+++ src/main/kotlin/com/mktbsh/ktdemo/KtdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.mktbsh.ktdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtdemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/mktbsh/ktdemo/KtdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mktbsh.ktdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtdemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
